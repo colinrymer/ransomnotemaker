e7e05e0e3a8374006a027723182113c2d77bfb35
ereinsmidt <eric@reinsmidt.com>
2012-02-03 02:36:19 -0500

    added quote from a dastardly character

--- web.rb
+++ web.rb
@@ -1,23 +1,22 @@
 require 'sinatra'
 require './lib/database'
-
+ 
 # Routes
 get '/' do
   erb :home
 end
-
-<<<<<<< HEAD
+ 
 post '/note' do
-	#TODO create new
-	@message = params[:message]
+  #TODO create note
+  @message = params[:message]
 	erb :note
-=======
+end
+
 not_found do
   erb :not_found
 end
-
+ 
 post '/new' do
   #TODO create new
   erb :note
->>>>>>> a86d7da05ff3a0ff9c6ba1ac0daa3d76abade179
 end
